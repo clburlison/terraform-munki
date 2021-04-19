4d387cf69da19a0af1ff2197a2adc6dd20779976
Clayton Burlison <git@clburlison.com>
2021-04-19 16:29:03 -0500

    feat: Support for basic auth

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "cf_domain_name" {
   description = "The cloudfront distribution point domain name"
-  value       = aws_cloudfront_distribution.munki_s3_distribution.domain_name
+  value = join("", aws_cloudfront_distribution.munki[*].domain_name, aws_cloudfront_distribution.munki_basic_auth[*].domain_name)
 }
 
 output "cf_hosted_zone_id" {
   description = "The cloudfront distribution point zone id"
-  value       = aws_cloudfront_distribution.munki_s3_distribution.hosted_zone_id
+  value = join("", aws_cloudfront_distribution.munki[*].hosted_zone_id, aws_cloudfront_distribution.munki_basic_auth[*].hosted_zone_id)
 }
 
--- variables.tf
+++ variables.tf
@@ -3,6 +3,16 @@ variable "alarm_arn" {
   default     = ""
 }
 
+variable "basic_auth_user" {
+  description = "Username for munki basic auth"
+  default     = ""
+}
+
+variable "basic_auth_password" {
+  description = "Password for munki basic auth"
+  default     = ""
+}
+
 variable "name" {
   description = "Name to be used on all resources as the identifier"
   default     = "munki"
@@ -14,6 +24,11 @@ variable "s3_bucket_name" {
 
 variable "s3_bucket_create" {
   description = "Set to true to create a new s3 bucket. If false you can reuse a current bucket"
+  default     = true
+}
+
+variable "server_side_makecatalogs" {
+  description = "Set to true to enable server side makecatalogs when s3 bucket changes happen"
   default     = true
 }
 
@@ -60,3 +75,62 @@ variable "tags" {
   default     = {}
 }
 
+variable "default_cache_behavior_min_ttl" {
+  default     = 0
+  description = "The minimum amount of time (in seconds) that you want objects to stay in CloudFront caches before CloudFront queries your origin to see whether the object has been updated."
+}
+
+variable "default_cache_behavior_default_ttl" {
+  default     = 86400
+  description = "The default amount of time (in seconds) that an object is in a CloudFront cache before CloudFront forwards another request in the absence of an Cache-Control max-age or Expires header."
+}
+
+variable "default_cache_behavior_max_ttl" {
+  default     = 31536000
+  description = "The maximum amount of time (in seconds) that an object is in a CloudFront cache before CloudFront forwards another request to your origin to determine whether the object has been updated."
+}
+
+variable "catalogs_ordered_cache_behavior_min_ttl" {
+  default     = 0
+  description = "The minimum amount of time (in seconds) that you want catalog objects to stay in CloudFront caches before CloudFront queries your origin to see whether the object has been updated."
+}
+
+variable "catalogs_ordered_cache_behavior_default_ttl" {
+  default     = 30
+  description = "The default amount of time (in seconds) that a catalog object is in a CloudFront cache before CloudFront forwards another request in the absence of an Cache-Control max-age or Expires header."
+}
+
+variable "catalogs_ordered_cache_behavior_max_ttl" {
+  default     = 60
+  description = "The maximum amount of time (in seconds) that a catalog object is in a CloudFront cache before CloudFront forwards another request to your origin to determine whether the object has been updated."
+}
+
+variable "manifests_ordered_cache_behavior_min_ttl" {
+  default     = 0
+  description = "The minimum amount of time (in seconds) that you want manifest objects to stay in CloudFront caches before CloudFront queries your origin to see whether the object has been updated."
+}
+
+variable "manifests_ordered_cache_behavior_default_ttl" {
+  default     = 30
+  description = "The default amount of time (in seconds) that a manifest object is in a CloudFront cache before CloudFront forwards another request in the absence of an Cache-Control max-age or Expires header."
+}
+
+variable "manifests_ordered_cache_behavior_max_ttl" {
+  default     = 60
+  description = "The maximum amount of time (in seconds) that a manifest object is in a CloudFront cache before CloudFront forwards another request to your origin to determine whether the object has been updated."
+}
+
+variable "icons_ordered_cache_behavior_min_ttl" {
+  default     = 0
+  description = "The minimum amount of time (in seconds) that you want icon objects to stay in CloudFront caches before CloudFront queries your origin to see whether the object has been updated."
+}
+
+variable "icons_ordered_cache_behavior_default_ttl" {
+  default     = 30
+  description = "The default amount of time (in seconds) that a icon object is in a CloudFront cache before CloudFront forwards another request in the absence of an Cache-Control max-age or Expires header."
+}
+
+variable "icons_ordered_cache_behavior_max_ttl" {
+  default     = 60
+  description = "The maximum amount of time (in seconds) that a icon object is in a CloudFront cache before CloudFront forwards another request to your origin to determine whether the object has been updated."
+}
