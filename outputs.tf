9bd8597a63dba19fca07c01b246df1cd3dbc8d63
Clayton Burlison <git@clburlison.com>
2021-04-19 16:31:22 -0500

    docs: Update README documentation

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,9 @@
 output "cf_domain_name" {
   description = "The cloudfront distribution point domain name"
-  value = join("", aws_cloudfront_distribution.munki[*].domain_name, aws_cloudfront_distribution.munki_basic_auth[*].domain_name)
+  value       = join("", aws_cloudfront_distribution.munki[*].domain_name, aws_cloudfront_distribution.munki_basic_auth[*].domain_name)
 }
 
 output "cf_hosted_zone_id" {
   description = "The cloudfront distribution point zone id"
-  value = join("", aws_cloudfront_distribution.munki[*].hosted_zone_id, aws_cloudfront_distribution.munki_basic_auth[*].hosted_zone_id)
+  value       = join("", aws_cloudfront_distribution.munki[*].hosted_zone_id, aws_cloudfront_distribution.munki_basic_auth[*].hosted_zone_id)
 }
-
