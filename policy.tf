ab0bfdaa39296697fd57f1892acee7a757225072
Clayton Burlison <git@clburlison.com>
2023-05-19 11:19:57 -0500

    fix: Add tags to more resources

--- policy.tf
+++ policy.tf
@@ -2,6 +2,12 @@ resource "aws_iam_policy" "munki-s3-rw" {
   name        = "${var.name}-s3-rw"
   path        = "/"
   description = "Munki s3 policy for read/write access to the s3 munki repo bucket. Used for automation purposes."
+  tags = merge(
+    var.tags,
+    {
+      "Name" = format("%s", var.name)
+    },
+  )
 
   policy = <<EOF
 {
